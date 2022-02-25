1ffe458065e8593cb2d3dc432fca8c78bbc839f3
parkjungkwan <pakjkwan@gmail.com>
2022-02-25 11:54:30 +0900

    11

--- main/java/net/zerotoweb/api/member/controller/MemberController.java
+++ main/java/net/zerotoweb/api/member/controller/MemberController.java
@@ -21,15 +21,41 @@ import java.util.Map;
  */
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService service;
 
 
-    @GetMapping("/member/bmi/{name}/{height}/{weight}")
-    public String getBmi(@PathVariable String name,
-                                      @PathVariable double height,
-                                      @PathVariable double weight){
+    @PostMapping("/bmi")
+    public String getBmi(@RequestBody MemberDTO member){
+        System.out.println(" 리액트에서 넘어온 이름: "+member.getName());
+        System.out.println(" 리액트에서 넘어온 키: "+member.getHeight());
+        System.out.println(" 리액트에서 넘어온 몸무게: "+member.getWeight());
+        return "BMI 는 정상";
+    }
+    @GetMapping("/grade")
+    public String getGrade(@PathVariable String name,
+                         @PathVariable double height,
+                         @PathVariable double weight){
+        System.out.println(" 리액트에서 넘어온 이름: "+name);
+        System.out.println(" 리액트에서 넘어온 키: "+height);
+        System.out.println(" 리액트에서 넘어온 몸무게: "+weight);
+        return "BMI 는 정상";
+    }
+    @GetMapping("/calc")
+    public String calc(@PathVariable String name,
+                           @PathVariable double height,
+                           @PathVariable double weight){
+        System.out.println(" 리액트에서 넘어온 이름: "+name);
+        System.out.println(" 리액트에서 넘어온 키: "+height);
+        System.out.println(" 리액트에서 넘어온 몸무게: "+weight);
+        return "BMI 는 정상";
+    }
+    @GetMapping("/login")
+    public String login(@PathVariable String name,
+                          @PathVariable double height,
+                          @PathVariable double weight){
         System.out.println(" 리액트에서 넘어온 이름: "+name);
         System.out.println(" 리액트에서 넘어온 키: "+height);
         System.out.println(" 리액트에서 넘어온 몸무게: "+weight);
