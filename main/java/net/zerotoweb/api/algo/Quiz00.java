c7a42feb973ce8ba7d60fe63e639a20e0e33d773
parkjungkwan <pakjkwan@gmail.com>
2022-03-03 18:11:10 +0900

    18

--- main/java/net/zerotoweb/api/algo/Quiz00.java
+++ main/java/net/zerotoweb/api/algo/Quiz00.java
@@ -1,4 +1,5 @@
 package net.zerotoweb.api.algo;
+import java.util.Random;
 import java.util.Scanner;
 /**
  * ================================
@@ -80,9 +81,73 @@ public class Quiz00 {
     }
     static void quiz01dice(){
         System.out.println("01 주사위");
+        // 홀수나올때까지 주사위굴려 합하는 프로그래밍
+        // Random random = new Random();
+        Random random = new Random();
+        int result = 0;
+        while(true) {
+            int r = random.nextInt(5)+1;
+            System.out.println(" dice value : "+ r);
+            if(r % 2 != 0) {
+                break;
+            }
+            result += r;
+        }
+        System.out.println(result);
+        // 6면인 주사위를 5회 굴려서 나온 합계를 구하시오.
+        // (int) (Math.random() * 최댓값) + 최솟값
+        result = 0;
+        for(int i=0; i<5; i++) {
+            double d = Math.random();
+            int temp = (int) (d * 6) +1;  // 랜덤으로 (1-6) 정수값
+            System.out.println(temp);
+            result += temp;
+        }
+        System.out.println("Sum : "+result);
+        // 플레이어(Math 랜덤값)와 컴퓨터(Random 랜덤값) 주사위 굴리기를 해 이기면
+        // "Win", 비기면 "Draw.", 지면 "Lose"를 출력하세요.
+        int player=(int)(Math.random()*6)+1;
+        System.out.println("플레이어 숫자"+player);
+        int com= new Random().nextInt(5)+1;
+        System.out.println("컴퓨터 숫자"+com);
+        String s = "Draw";
+        if(player!=com) s = (player>com) ? "Win" : "Lose";
+        System.out.println(s);
     }
     static void quiz02rps(){
         System.out.println("02 가위바위보");
+        System.out.println(" ----------- 1 -------------");
+        int a = (int)(Math.random()*3)+1;
+        int b = (int)(Math.random()*3)+1;
+        System.out.println((a == b) ? "Draw" : (b > (a+1)%3) ? "Win":"lose");
+        System.out.println(" ----------- 2 -------------");
+        a = (int)(Math.random()*3)+1;
+        b = (int)(Math.random()*3)+1;
+        System.out.println(a);
+        System.out.println(b);
+        String s = "";
+        if (a == b) s= "Draw";
+		/*
+		 2%3 2 3
+		 3%3 0 1
+		 1%3 1 2
+		 * */
+        else s = (b > (a+1)%3) ? "Win":"lose";
+        System.out.println(s);
+        System.out.println(" ----------- 3 -------------");
+        String[] arr = {"가위", "바위", "보", "Draw", "Win", "Lose"};
+        int[] arr2 = new int[2];
+        for(int i =0 ; i<arr2.length; i++) arr2[i] = (int)(Math.random()*3)+1;
+        int p = arr2[0], c = arr2[1];
+        System.out.printf("Player : %s %d\n",arr[p-1], p);
+        System.out.printf("Computer : %s %d\n",arr[c-1], c);
+        int i = 3;
+        switch(Math.abs(p - c)) {
+            case 1: i = (p > c) ? 5 : 4;
+            case 2: i = (p > c) ? 4 : 5;
+            default: break;
+        }
+        System.out.printf("Player: %s", arr[i]);
     }
     static void quiz03prime(){
         System.out.println("03 소수");
@@ -104,6 +169,53 @@ public class Quiz00 {
     }
     static void quiz09gugudan(){
         System.out.println("09 구구단");
+        System.out.println(" --------- 1 --------------");
+        for(int k = 2; k < 10; k+=4) {
+            for (int i = 1; i < 10; i++) {
+                for (int j = k; j < k+4; j++) {
+                    System.out.print(j + "*" + i+ "=" +(j*i)+"\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println(" --------- 2 --------------");
+        for (int i = 1; i < 10; i++) {
+            for (int j = 2; j < 6; j++) {
+                System.out.print(j + "*" + i+ "=" +(j*i)+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 2; i < 10; i++) {
+            for (int j = 6; j < 10; j++) {
+                System.out.print(j + "*" + i+ "=" +(j*i)+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println(" --------- 3 --------------");
+        /*
+        for(i 1 10 1)
+        for(j 2 6 1)
+        j * i = j * i
+        for(i 1 10 1)
+        for(j 6 10 1)
+        j * i = j * i
+        에서 같은 구문이 반복되므로, 다시 for 를 사용한다.
+        for(k 2, 10, 4)
+        for(i 1, 10, 1)
+        for(j k, k+4, 1)
+        j * i = (j * i)
+         * */
+        for (int dan = 2; dan < 10; dan += 4) { //구구단은 2단부터 시작.
+            for (int i = 1; i <= 9; i++) { // 1 ~ 9 까지 곱이 나오도록 조건잡기
+                for (int j = dan; j < dan + 4; j++) {
+                    System.out.print(j + "*" + i + "=" + (j * i) + "\t");
+                }
+                System.out.print("\n"); // 4개씩 출력 되어야 하므로 j*i 값 4개가 출력된 후 엔터
+            }
+            System.out.println("\n"); //4개의 단(2단~5단)이 모두 출력 된 후, 엔터
+        }
     }
 
     public static void main(String[] args) {
